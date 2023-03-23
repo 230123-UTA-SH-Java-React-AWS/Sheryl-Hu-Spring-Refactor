a25fca48d322b72f9935b4895162d2a8a725357b
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2023-03-23 15:56:15 -0700

    Created method to find messages by particular account

--- refactor/src/main/java/com/revature/refactor/Repository/MessageRepository.java
+++ refactor/src/main/java/com/revature/refactor/Repository/MessageRepository.java
@@ -1,4 +1,9 @@
 package com.revature.refactor.Repository;
 
-public interface MessageRepository {
+import com.revature.refactor.Model.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.*;
+
+public interface MessageRepository extends JpaRepository<Message, Integer> {
+    List<Message> findAllMessagesByPostedBy(Integer postedBy);
 }
