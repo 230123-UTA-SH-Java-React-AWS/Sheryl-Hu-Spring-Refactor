9109b7e6b35af55c4fb562d9be7933ba3965be98
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2023-03-23 15:36:30 -0700

    Created controller endpoints for registering new accounts and logging in

--- refactor/src/main/java/com/revature/refactor/Controller/AccountController.java
+++ refactor/src/main/java/com/revature/refactor/Controller/AccountController.java
@@ -1,4 +1,36 @@
 package com.revature.refactor.Controller;
 
+import com.revature.refactor.Model.Account;
+import com.revature.refactor.Service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AccountController {
+    @Autowired
+    AccountService accountService;
+
+    @PostMapping("/register")
+    public ResponseEntity<Account> createNewAccount(@RequestBody Account account) {
+        Account newAccount = accountService.createAccount(account);
+        if (newAccount != null) {
+            return ResponseEntity.status(200).body(newAccount);
+        } else {
+            return ResponseEntity.status(400).body(null);
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Account> login(@RequestBody Account account) {
+        Account loginAttempt = accountService.loginAccount(account);
+        if (loginAttempt != null) {
+            return ResponseEntity.status(200).body(loginAttempt);
+        } else {
+            return ResponseEntity.status(401).body(null);
+        }
+    }
 }
