ff722446ff8a49cac483efa5241a16c01adf1bd6
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2023-03-23 16:22:20 -0700

    Created service methods to create new message, retrieve all messages, and retrieve a message by ID

--- refactor/src/main/java/com/revature/refactor/Service/MessageService.java
+++ refactor/src/main/java/com/revature/refactor/Service/MessageService.java
@@ -1,4 +1,35 @@
 package com.revature.refactor.Service;
 
+import com.revature.refactor.Repository.AccountRepository;
+import com.revature.refactor.Repository.MessageRepository;
+import com.revature.refactor.Model.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.*;
+
 public class MessageService {
+    @Autowired
+    private MessageRepository messageRepo;
+
+    @Autowired
+    private AccountRepository accountRepo;
+
+    public Message createNewMessage(Message message) {
+        if (accountRepo.findById(message.getPostedBy()).isPresent() && message.getMessageText().length() != 0) {
+            return messageRepo.save(message);
+        } else {
+            return null;
+        }
+    }
+
+    public List<Message> getAllMessages() {
+        return messageRepo.findAll();
+    }
+
+    public Optional<Message> getMessageById(int messageId) {
+        if (!messageRepo.findById(messageId).isPresent()) {
+            return Optional.empty();
+        } else {
+            return messageRepo.findById(messageId);
+        }
+    }
 }
