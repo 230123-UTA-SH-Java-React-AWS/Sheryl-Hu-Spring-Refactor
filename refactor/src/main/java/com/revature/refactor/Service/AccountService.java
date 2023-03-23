ace34569fd0d8758e01339643bf09c446c4391cb
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2023-03-23 15:32:46 -0700

    Created service methods to create new account and login

--- refactor/src/main/java/com/revature/refactor/Service/AccountService.java
+++ refactor/src/main/java/com/revature/refactor/Service/AccountService.java
@@ -1,4 +1,28 @@
 package com.revature.refactor.Service;
 
+import com.revature.refactor.Model.Account;
+import com.revature.refactor.Repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AccountService {
+    @Autowired
+    public AccountRepository accountRepo;
+
+    public Account createAccount(Account account) {
+        if (account.getUsername() != "" && account.getPassword().length() >= 4 && accountRepo.findAccountByUsername(account.getUsername()) == null) {
+            return accountRepo.save(account);
+        } else {
+            return null;
+        }
+    }
+
+    public Account loginAccount(Account account) {
+        if (accountRepo.findAccountByUsername(account.getUsername()) != null) {
+            return accountRepo.findAccountByUsernameAndPassword(account.getUsername(), account.getPassword());
+        } else {
+            return null;
+        }
+    }
 }
