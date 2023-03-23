f1ac687b27c139910f376b220cd2d30c476ed588
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2023-03-23 13:46:07 -0700

    Created message model

---
+++ refactor/src/main/java/com/revature/refactor/Model/Message.java
@@ -0,0 +1,73 @@
+package com.revature.refactor.Model;
+
+import java.util.Objects;
+
+public class Message {
+    private int messageId;
+    private int postedBy;
+    private String messageText;
+    private long timePostedEpoch;
+
+    public Message(int messageId, int postedBy, String messageText,
+                   long timePostedEpoch) {
+        this.messageId = messageId;
+        this.postedBy = postedBy;
+        this.messageText = messageText;
+        this.timePostedEpoch = timePostedEpoch;
+    }
+
+    public int getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(int messageId) {
+        this.messageId = messageId;
+    }
+
+    public int getPostedBy() {
+        return postedBy;
+    }
+
+    public void setPostedBy(int postedBy) {
+        this.postedBy = postedBy;
+    }
+
+    public String getMessageText() {
+        return messageText;
+    }
+
+    public void setMessageText(String messageText) {
+        this.messageText = messageText;
+    }
+
+    public long getTimePostedEpoch() {
+        return timePostedEpoch;
+    }
+
+    public void setTimePostedEpoch(long timePostedEpoch) {
+        this.timePostedEpoch = timePostedEpoch;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "messageId=" + messageId +
+                ", postedBy=" + postedBy +
+                ", messageText='" + messageText + '\'' +
+                ", timePostedEpoch=" + timePostedEpoch +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Message message = (Message) o;
+        return messageId == message.messageId && postedBy == message.postedBy && timePostedEpoch == message.timePostedEpoch && Objects.equals(messageText, message.messageText);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(messageId, postedBy, messageText, timePostedEpoch);
+    }
+}
