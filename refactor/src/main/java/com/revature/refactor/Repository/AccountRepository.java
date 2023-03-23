bebd040b8e386bf7189c7b3f28fa4b8567e46754
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2023-03-23 15:31:59 -0700

    Added custom methods to find user by credentials

--- refactor/src/main/java/com/revature/refactor/Repository/AccountRepository.java
+++ refactor/src/main/java/com/revature/refactor/Repository/AccountRepository.java
@@ -1,4 +1,11 @@
 package com.revature.refactor.Repository;
 
-public interface AccountRepository {
+import com.revature.refactor.Model.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Integer> {
+    Account findAccountByUsernameAndPassword(String username, String password);
+    Account findAccountByUsername(String username);
 }
