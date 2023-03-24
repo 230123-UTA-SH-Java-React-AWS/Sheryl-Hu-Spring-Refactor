8a7bf2779a6a4ca89d04754835ccbeec68208c98
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2023-03-24 07:37:08 -0700

    Added route to retrieve all messages by a particular account

--- refactor/src/main/java/com/revature/refactor/Controller/AccountController.java
+++ refactor/src/main/java/com/revature/refactor/Controller/AccountController.java
@@ -1,18 +1,23 @@
 package com.revature.refactor.Controller;
 
 import com.revature.refactor.Model.Account;
+import com.revature.refactor.Model.Message;
 import com.revature.refactor.Service.AccountService;
+import com.revature.refactor.Service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
 
 
 @RestController
 public class AccountController {
     @Autowired
     AccountService accountService;
+
+    @Autowired
+    MessageService messageService;
 
     @PostMapping("/register")
     public ResponseEntity<Account> createNewAccount(@RequestBody Account account) {
@@ -32,5 +37,10 @@ public class AccountController {
         } else {
             return ResponseEntity.status(401).body(null);
         }
+    }
+
+    @GetMapping("/accounts/{account_id}/messages")
+    public List<Message> getAllMessagesOfAccount(@PathVariable int account_id) {
+        return messageService.getMessagesByAccount(account_id);
     }
 }
