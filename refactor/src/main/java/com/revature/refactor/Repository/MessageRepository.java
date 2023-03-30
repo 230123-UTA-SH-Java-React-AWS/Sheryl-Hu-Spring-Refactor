8cdb5b1ab165d1edc0296cb133958f659b7de976
Sheryl H <sherylhu@proton.me>
2023-03-30 08:25:43 -0700

    Delete refactor directory

--- refactor/src/main/java/com/revature/refactor/Repository/MessageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.revature.refactor.Repository;
-
-import com.revature.refactor.Model.Message;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.*;
-
-public interface MessageRepository extends JpaRepository<Message, Integer> {
-    List<Message> findAllMessagesByPostedBy(Integer postedBy);
-}
